4f827d76d8cb9661d73f4679959fb817177456f8
Roger Williams <roger.kwilliams93@gmail.com>
2017-03-31 13:23:13 -0400

    Added test file
    ----

---
+++ src/cs1302/gsu/edu/emperorsofspades/MainOne.java
@@ -0,0 +1,10 @@
+package cs1302.gsu.edu.emperorsofspades;
+
+/**
+ * Created by roger on 3/31/2017.
+ */
+public class MainOne {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+    }
+}
