37a16e523ecf2194165eee6ba6f40229cae1ca9e
mahty24 <mahty24@gmail.com>
2017-04-04 03:01:49 -0400

    Revert "Game Engine"
    This reverts commit e584205fd9dc5185a2bc38e0ec4f7dfde33a9b3a.

--- src/edu/gsu/csc1302/emperorsofspades/Round.java
+++
@@ -1,8 +0,0 @@
-package edu.gsu.csc1302.emperorsofspades;
-
-public class Round {
-	private int round = 0;
-	Round(){
-		round = round + 1;
-	}
-}
--- src/edu/gsu/csc1302/emperorsofspades/player/Player.java
+++ src/edu/gsu/csc1302/emperorsofspades/player/Player.java
@@ -1,7 +1,5 @@
 package edu.gsu.csc1302.emperorsofspades.player;
 
-import edu.gsu.csc1302.coll1.instr.ArrayListDeck;
-
 /**
  * Simulates a generic player in the game.
  * @author Roger Williams
@@ -10,8 +8,7 @@ public abstract class Player {
     /**
      * The player's name.
      */
-    private String name;
-    private ArrayListDeck hand;
+    private final String name;
 
     /**
      * Class constructor.
@@ -19,37 +16,16 @@ public abstract class Player {
      */
     public Player(final String name) {
         this.name = name;
-        hand = null;
-        
     }
 
     /**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @param hand the hand to set
-	 */
-	public void setHand(ArrayListDeck hand) {
-		this.hand = hand;
-	}
-
-	/**
      * Returns the player's name.
      * @return player's name
      */
     public String getName() {
         return name;
     }
-    /**
-     * returns the Deck of the player.
-     */
-    public  ArrayListDeck getHand() {
-        return hand;
-    }
+
     /**
      * Print-friendly string.
      * @return
