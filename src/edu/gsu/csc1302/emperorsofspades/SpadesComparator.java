fc7eeed37bb900238f0dd96d15e87437c85f7cd0
mahty24 <mahty24@gmail.com>
2017-04-04 11:37:50 -0400

    Reerted the changes
    deleted changes on the player class.

---
+++ src/edu/gsu/csc1302/emperorsofspades/SpadesComparator.java
@@ -0,0 +1,12 @@
+package edu.gsu.csc1302.emperorsofspades;
+
+import edu.gsu.csc1302.emperorsofspades.instructorsolutions.Card.Suit;
+import edu.gsu.csc1302.emperorsofspades.instructorsolutions.InstrSpadesComparator;
+
+public class SpadesComparator extends InstrSpadesComparator {
+
+	public SpadesComparator(Suit leadSuit) {
+		super(leadSuit);
+	}
+
+}
