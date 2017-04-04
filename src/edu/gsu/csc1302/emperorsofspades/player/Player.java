e584205fd9dc5185a2bc38e0ec4f7dfde33a9b3a
mahty24 <mahty24@gmail.com>
2017-04-04 02:56:58 -0400

    Game Engine
    Added startRound, startNewRound, playHand methods that can do many of
    the game classes requirmets.
    It needs the Player class to have a filed to track its team, and the
    team class to have a filed to track its score.

--- src/edu/gsu/csc1302/emperorsofspades/player/Player.java
+++ src/edu/gsu/csc1302/emperorsofspades/player/Player.java
@@ -1,5 +1,7 @@
 package edu.gsu.csc1302.emperorsofspades.player;
 
+import edu.gsu.csc1302.coll1.instr.ArrayListDeck;
+
 /**
  * Simulates a generic player in the game.
  * @author Roger Williams
@@ -8,7 +10,8 @@ public abstract class Player {
     /**
      * The player's name.
      */
-    private final String name;
+    private String name;
+    private ArrayListDeck hand;
 
     /**
      * Class constructor.
@@ -16,16 +19,37 @@ public abstract class Player {
      */
     public Player(final String name) {
         this.name = name;
+        hand = null;
+        
     }
 
     /**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param hand the hand to set
+	 */
+	public void setHand(ArrayListDeck hand) {
+		this.hand = hand;
+	}
+
+	/**
      * Returns the player's name.
      * @return player's name
      */
     public String getName() {
         return name;
     }
-
+    /**
+     * returns the Deck of the player.
+     */
+    public  ArrayListDeck getHand() {
+        return hand;
+    }
     /**
      * Print-friendly string.
      * @return
